daa73a91102b8eae49ed144c66275e789bff41ee
PhongGodFather <dinhquangphong365@gmail.com>
2023-12-19 17:40:09 +0700

    update a bit at port in index,js for easier use

--- src/index.js
+++ src/index.js
@@ -55,5 +55,5 @@ app.get('/', (req, res) =>{
 })
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on port http://localhost:${PORT}`)
   });
