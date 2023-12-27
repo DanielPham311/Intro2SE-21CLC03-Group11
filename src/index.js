e4e9c6b5cbbe88fe3abec9d1fa0d4c80c97572c3
Tăng Tường Thoại <thoaitang24603@gmail.com>
2023-12-27 09:12:25 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
-const express = require('express');
+const express = require("express");
 const PORT = process.env.PORT || 8080;
 const app = express();
-const expressHbs = require('express-handlebars');
+const expressHbs = require("express-handlebars");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
-const passport = require('passport');
-const authenticateRoute = require('./routers/authenRouter');
-const {isAuth, isAdmin} = require('./middlewares/authenticateMiddleware')
-const path = require('path');
+const passport = require("passport");
+const authenticateRoute = require("./routers/authenRouter");
+const { isAuth, isAdmin } = require("./middlewares/authenticateMiddleware");
+const path = require("path");
+
 // set the view engine
-app.engine('hbs', expressHbs.engine({
+app.engine(
+  "hbs",
+  expressHbs.engine({
     layoutsDir: __dirname + "/views/layouts",
     partialDir: __dirname + "/views/partials",
     defaultLayout: "layout",
     extname: "hbs",
-}));
+  })
+);
 
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs");
 
 // Serve static files from the 'public' folder
-app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, "static")));
 
 // Cau hinh cho phep doc du lieu gui len bang phuong thuc POST
 app.use(express.json());
@@ -29,33 +33,31 @@ app.use(cookieParser("COOKIE_SECRET"));
 
 // THiết lập sử dụng session và lưu trữ session trên redis
 app.use(
-  session(
-    {
-      secret: "SESSION_SECRET",
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        secure: false, // if true only transmit cookie over https
-        httpOnly: true, // prevent client side js from reading the cooking
-        maxAge: 1000 * 60 * 60 // maximum age 1 hour
-      }
-    }
-  )
+  session({
+    secret: "SESSION_SECRET",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false, // if true only transmit cookie over https
+      httpOnly: true, // prevent client side js from reading the cooking
+      maxAge: 1000 * 60 * 60, // maximum age 1 hour
+    },
+  })
 );
 
 /*
 PASSPORT AUTHENTICATION
 */
-require('./config/passport');
+require("./config/passport");
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(authenticateRoute);
 app.use(isAuth);
 
-app.get('/', (req, res) =>{
-    res.render('index');
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`)
-  });
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
