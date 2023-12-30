23153c1244017b82e690749d360c48ce706c8230
Tăng Tường Thoại <thoaitang24603@gmail.com>
2023-12-30 12:59:50 +0700

    fix layout component

--- src/controllers/MovieController.js
+++ src/controllers/MovieController.js
@@ -1,13 +1,26 @@
+const { move } = require('../routers/WatchMovieRouter');
+const MovieService = require('../services/MovieService')
+
 const controller = {};
 
-controller.movie_detail = async (req, res, next) => {
-    res.render('movieScreen', {
-       layout: 'functional_layout',
-       css_file_name: 'movieWatching' 
-    });
+controller.movie_detail = async (req, res) => {
+    const movieId = req.params.movie_id;
+    console.log(movieId);
+    try {
+        const movie = await MovieService.getMovieById(movieId);
+        console.log(movie);
+        res.render('movieScreen', {
+            layout: 'functional_layout',
+            css_file_name: 'movieWatching',
+            movie : movie,
+         });
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 controller.search = async (req, res) => {
+    
     res.json(req.user);
 }
 
