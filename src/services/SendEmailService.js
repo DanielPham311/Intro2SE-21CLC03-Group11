37288ee52f1a85b8960cd500e575338691c4ea9f
DanielPham311 <phamquocduy3112003@gmail.com>
2024-01-04 19:50:54 +0700

    updated mail message

--- src/services/SendEmailService.js
+++ src/services/SendEmailService.js
@@ -14,7 +14,19 @@ MailService.sendEmail = async (userEmail, newPassword) => {
     from: "noreply.netflex@gmail.com",
     to: userEmail,
     subject: "Password Reset",
-    text: `Your new password is: ${newPassword}`,
+    text: `
+    Dear beloved user of Netflex,
+    
+    You have received this message since you requested a password reset on our website.
+
+    Your new password is: ${newPassword}
+    
+    Please hold on to it very carefully and take actions to change your password as soon as possible
+    If you did not request this, please send us an email at netflex@gmail.com
+    
+    We wish you a wonderful experience with Netflex
+    Best Regards,
+    Netflex crew`,
   };
 
   return transporter.sendMail(mailOptions);
